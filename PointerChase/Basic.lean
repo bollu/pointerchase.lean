a973da4c1f38560c570aceda7c1455693a204721
Siddharth Bhat <siddu.druid@gmail.com>
2024-10-30 00:54:25 -0500

    chore: Traversable -> Representable

--- PointerChase/Basic.lean
+++ PointerChase/Basic.lean
@@ -1,4 +1,4 @@
-import PointerChase.Traversable
+import PointerChase.Representable
 import Std
 open Std
 
@@ -7,14 +7,14 @@ namespace PointerChase
 
 
 /-- Coerce any node to a value node, that forgets the pointer data. -/
-instance [Functor NodeF] [Foldable NodeF] : CoeOut (NodeF α) (NodeF Unit) where
+instance [Functor NodeF] [Representable NodeF] : CoeOut (NodeF α) (NodeF Unit) where
   coe := Functor.fillUnit
 
 /-- A Node has fields as natural numbers. -/
 abbrev PreNode (NodeF : Type → Type) := NodeF Nat
 
-def PreNode.LessThan [Functor NodeF] [Foldable NodeF] (n : PreNode NodeF) (ix : Nat) : Prop := 
-  Foldable.ForAll n (fun v => v < ix)
+def PreNode.LessThan [Functor NodeF] [Representable NodeF] (n : PreNode NodeF) (ix : Nat) : Prop := 
+  Representable.ForAll n (fun v => v < ix)
 
 
 /-
@@ -30,36 +30,36 @@ theorem PreNode.LessThan_var :
     exact h
 -/
 
-structure Nodes (NodeF) [Functor NodeF] [Foldable NodeF] where
+structure Nodes (NodeF) [Functor NodeF] [Representable NodeF] where
   heap : Array (PreNode NodeF)
   ordered : ∀ {i : Nat} (hi : i < heap.size), heap[i].LessThan i
 
 
 /-- A handle to a node in a nodes data structure -/
-structure Nodes.Ptr [Functor NodeF] [Foldable NodeF] (nodes : Nodes NodeF) where
+structure Nodes.Ptr [Functor NodeF] [Representable NodeF] (nodes : Nodes NodeF) where
   ix : Nat
   hix : ix < nodes.heap.size
 
 @[simp]
-theorem Nodes.Ptr.lt_heap_size [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (p : nodes.Ptr) :
+theorem Nodes.Ptr.lt_heap_size [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (p : nodes.Ptr) :
   p.ix < nodes.heap.size := p.hix
 
-def Nodes.Ptr.Lt {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF}
+def Nodes.Ptr.Lt {NodeF : Type → Type} [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF}
   (h₁ h₂ : nodes.Ptr) : Prop := h₁.ix < h₂.ix
 
 @[simp]
-def Nodes.Ptr.Lt_trans {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (p q r : nodes.Ptr) :
+def Nodes.Ptr.Lt_trans {NodeF : Type → Type} [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (p q r : nodes.Ptr) :
     p.Lt q → q.Lt r → p.Lt r := by
   simp [Nodes.Ptr.Lt]
   omega
 
 /-- Ptr.Lt is transitive -/
-instance {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} :
+instance {NodeF : Type → Type} [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} :
     Trans (Nodes.Ptr.Lt) (Nodes.Ptr.Lt) (Nodes.Ptr.Lt (nodes := nodes)) where
   trans p q := by apply Nodes.Ptr.Lt_trans <;> assumption
 
 /-- The size of the node pointer is an accessibility relation. -/
-def Nodes.Ptr.Lt_Acc {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (n : nodes.Ptr) :
+def Nodes.Ptr.Lt_Acc {NodeF : Type → Type} [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (n : nodes.Ptr) :
     Acc (Nodes.Ptr.Lt) n :=
   match hn : n.ix with
   | 0 => by
@@ -73,12 +73,12 @@ def Nodes.Ptr.Lt_Acc {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {n
     apply Lt_Acc
 termination_by n.ix
 
-def Nodes.Ptr.Lt_Wf {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF]
+def Nodes.Ptr.Lt_Wf {NodeF : Type → Type} [Functor NodeF] [Representable NodeF]
     {nodes : Nodes NodeF} : WellFounded (Nodes.Ptr.Lt (nodes := nodes)) := by
   constructor
   exact Nodes.Ptr.Lt_Acc
 
-instance (priority := high) {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF]
+instance (priority := high) {NodeF : Type → Type} [Functor NodeF] [Representable NodeF]
     {nodes : Nodes NodeF}  : WellFoundedRelation (Nodes.Ptr nodes) where
   rel := Nodes.Ptr.Lt
   wf := Nodes.Ptr.Lt_Wf
@@ -86,15 +86,15 @@ instance (priority := high) {NodeF : Type → Type} [Functor NodeF] [Foldable No
 
 
 /-- Get a prenode from the set of nodes. -/
-def Nodes.getPreNode {NodeF : Type → Type} [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (ptr : nodes.Ptr) : PreNode NodeF :=
+def Nodes.getPreNode {NodeF : Type → Type} [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (ptr : nodes.Ptr) : PreNode NodeF :=
   have := ptr.hix
   nodes.heap[ptr.ix]
 
 /-- An interned node -/
-def Nodes.Inode [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF}  := NodeF nodes.Ptr
+def Nodes.Inode [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF}  := NodeF nodes.Ptr
 
 /-- Erase all pointers in the inode -/
-def Nodes.Inode.erasePointers [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (inode : nodes.Inode) : NodeF Unit := Functor.fillUnit inode
+def Nodes.Inode.erasePointers [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (inode : nodes.Inode) : NodeF Unit := Functor.fillUnit inode
 
 
 /-- A pointer that is derived from a base pointer,
@@ -102,32 +102,32 @@ and is thus guaranteed to be smaller.
 TODO: it's always possible to keep a sentinel pointer, and derive all pointers from the sentinel?
 TODO: The ordering should not be in terms of Nat, but rather directly in terms of the naperian structure?
 -/
-structure Nodes.Ptr.DerivedPtr {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF} (p : nodes.Ptr) extends nodes.Ptr where
+structure Nodes.Ptr.DerivedPtr {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF} (p : nodes.Ptr) extends nodes.Ptr where
   hlt : toPtr.Lt p
 
 /-- An interned node, whose pointers are derived from a base pointer. -/
-def Nodes.Ptr.DerivedInode {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr) :=
+def Nodes.Ptr.DerivedInode {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr) :=
   NodeF p.DerivedPtr
 
 
-instance {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} :
+instance {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} :
     CoeOut (Nodes.Ptr.DerivedPtr p) (nodes.Ptr) where
   coe dp := dp.toPtr
 
 /-- Convert a derived INode into a regular Inode -/
-def Nodes.Ptr.DerivedInode.toInode [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} (dinode : p.DerivedInode) : nodes.Inode :=
+def Nodes.Ptr.DerivedInode.toInode [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} (dinode : p.DerivedInode) : nodes.Inode :=
   Functor.map (fun (q : p.DerivedPtr) => (↑q : nodes.Ptr)) dinode
 
-instance {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} :
+instance {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  {p : nodes.Ptr} :
     CoeOut (p.DerivedInode) (nodes.Inode) where
   coe dinode := dinode.toInode
 
 @[simp]
-theorem Nodes.Ptr.DerivedPtr.Lt {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr)
+theorem Nodes.Ptr.DerivedPtr.Lt {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr)
   (q : p.DerivedPtr) : q.Lt p := q.hlt
 
 
-def Nodes.Ptr.mkDerivedINode {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF} (p : nodes.Ptr) (n : PreNode NodeF)
+def Nodes.Ptr.mkDerivedINode {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF} (p : nodes.Ptr) (n : PreNode NodeF)
     (hn : n.LessThan p.ix) :  p.DerivedInode :=
   have hpix := p.hix
   Functor.map (fun hix => {
@@ -137,10 +137,10 @@ def Nodes.Ptr.mkDerivedINode {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : 
     hlt := by 
       simp [Nodes.Ptr.Lt]
       omega
-  }) (Foldable.attachForAll hn) 
+  }) (Representable.attachForAll hn) 
 
 /-- Get the value at the handle. -/
-def Nodes.Ptr.val {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr) : p.DerivedInode :=
+def Nodes.Ptr.val {NodeF} [Functor NodeF] [Representable NodeF]  {nodes : Nodes NodeF}  (p : nodes.Ptr) : p.DerivedInode :=
   have hinbounds := p.hix
   let v := nodes.heap[p.ix]
   have := nodes.ordered hinbounds
@@ -149,7 +149,7 @@ def Nodes.Ptr.val {NodeF} [Functor NodeF] [Foldable NodeF]  {nodes : Nodes NodeF
 
 
 /-- An empty heap -/
-def Nodes.empty [Functor NodeF] [Foldable NodeF] : Nodes NodeF := {
+def Nodes.empty [Functor NodeF] [Representable NodeF] : Nodes NodeF := {
   heap := #[],
   ordered := by
     intros i hi
@@ -157,15 +157,15 @@ def Nodes.empty [Functor NodeF] [Foldable NodeF] : Nodes NodeF := {
 }
 
 
-def Nodes.Inode.toPreNode [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (n : nodes.Inode) : PreNode NodeF :=
+def Nodes.Inode.toPreNode [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (n : nodes.Inode) : PreNode NodeF :=
   Functor.map (fun p => p.ix) n
 
 @[simp]
-theorem Nodes.Inode_inboundes [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (n : nodes.Inode) :
+theorem Nodes.Inode_inboundes [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (n : nodes.Inode) :
     n.toPreNode.LessThan nodes.heap.size := by
   simp [PreNode.LessThan, Nodes.Inode.toPreNode]
 
-def Nodes.lastPtr [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (h : nodes.heap.size > 0) : nodes.Ptr :=
+def Nodes.lastPtr [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (h : nodes.heap.size > 0) : nodes.Ptr :=
   { ix := nodes.heap.size - 1,
     hix := by
       have := h
@@ -173,16 +173,16 @@ def Nodes.lastPtr [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (h : no
   }
 
 @[simp]
-theorem Nodes.ix_lastPtr  [Functor NodeF] [Foldable NodeF] {nodes : Nodes NodeF} (h : nodes.heap.size > 0) : 
+theorem Nodes.ix_lastPtr  [Functor NodeF] [Representable NodeF] {nodes : Nodes NodeF} (h : nodes.heap.size > 0) : 
   (nodes.lastPtr h).ix = nodes.heap.size - 1 := rfl
 
 
-structure Nodes.insert.Result (NodeF : Type → Type) [Functor NodeF] [Foldable NodeF] where
+structure Nodes.insert.Result (NodeF : Type → Type) [Functor NodeF] [Representable NodeF] where
   nodes : Nodes NodeF
   ptr : nodes.Ptr
 
 /-- Given a well formed, interned node, insert it into the set of nodes. -/
-def Nodes.insert [Functor NodeF] [Foldable NodeF] (nodes : Nodes NodeF) (n : nodes.Inode) : Nodes.insert.Result NodeF :=
+def Nodes.insert [Functor NodeF] [Representable NodeF] (nodes : Nodes NodeF) (n : nodes.Inode) : Nodes.insert.Result NodeF :=
   let nodes' := { nodes with
     heap := nodes.heap.push n.toPreNode,
     ordered := by
@@ -206,7 +206,7 @@ def Nodes.insert [Functor NodeF] [Foldable NodeF] (nodes : Nodes NodeF) (n : nod
 
 /-- Grabbing the values gives us equal results -/
 @[simp]
-theorem Nodes.val_ptr_insert_eq [Functor NodeF] [Foldable NodeF] [LawfulFunctor NodeF]
+theorem Nodes.val_ptr_insert_eq [Functor NodeF] [Representable NodeF] [LawfulFunctor NodeF]
   (nodes : Nodes NodeF) (n : nodes.Inode) :
   ((nodes.insert n).ptr.val.toInode.erasePointers) = n.erasePointers := by
   simp [Inode.erasePointers, Nodes.Ptr.DerivedInode.toInode, Nodes.Ptr.val, Nodes.insert, Ptr.mkDerivedINode, Inode.toPreNode]
--- PointerChase/Representable.lean
+++ PointerChase/Representable.lean
@@ -2,7 +2,7 @@
 
 -- What I actually want is an indexed container, aka a representable functor.
 -- We encode the representable functor in terms of a lens-like API.
-class Foldable (f : Type → Type) [Functor f]  where
+class Representable (f : Type → Type) [Functor f]  where
   Ix : Type -- return the size representing the number of slots in the container.
   /-- Get the kth value -/
   get : f α → (Ix → α)
@@ -13,50 +13,50 @@ class Foldable (f : Type → Type) [Functor f]  where
   build_get (container : f α) : build (get container) = container
   get_map (container : f α) (fn : α → β) : get (fn <$> container) ix = fn (get container ix) 
   
-attribute [simp] Foldable.get_map
-attribute [simp] Foldable.get_build
-attribute [simp] Foldable.build_get
+attribute [simp] Representable.get_map
+attribute [simp] Representable.get_build
+attribute [simp] Representable.build_get
 
 /-- Get is injective, because it's a bijection -/
-theorem Foldable.get_inj [Functor f] [Foldable f] (c c' : f α) (h : get c = get c') : c = c' := by
+theorem Representable.get_inj [Functor f] [Representable f] (c c' : f α) (h : get c = get c') : c = c' := by
   have : build (get c) = build (get c') := by simp [h]
   simp only [build_get] at this
   exact this
 
 /-- Build is injective, because it's a bijection -/
-theorem Foldable.build_inj [Functor f] [Foldable f] (ix2val ix2val' : Ix f → α) (h : build ix2val = build ix2val') : ix2val = ix2val' := by
+theorem Representable.build_inj [Functor f] [Representable f] (ix2val ix2val' : Ix f → α) (h : build ix2val = build ix2val') : ix2val = ix2val' := by
   have : get (build ix2val) = get (build ix2val') := by simp [h]
   simp only [get_build] at this
   exact this
 
 /-- Dual of get_map in terms of build. Mapping a function after building is the same as mapping it before building. -/
 @[simp]
-theorem Foldable.map_build [Functor f] [Foldable f] (ix2val : Ix f → α) (fn : α → β) : 
+theorem Representable.map_build [Functor f] [Representable f] (ix2val : Ix f → α) (fn : α → β) : 
     fn <$> (build ix2val) = build (fn ∘ ix2val) := by
-  apply Foldable.get_inj
+  apply Representable.get_inj
   funext ix
   simp
 
 /-- Predicate holds for all indexes -/
-def Foldable.ForAll [Functor f] [Foldable f] (container : f α) (p : α → Prop) : Prop := 
+def Representable.ForAll [Functor f] [Representable f] (container : f α) (p : α → Prop) : Prop := 
   ∀ (ix : Ix f), p (get container ix)
 
 @[simp]
-theorem Foldable.ForAll.toForall [Functor f] [Foldable f] : ForAll container p ↔ ∀ (ix : Ix f), p (get container ix) := by
+theorem Representable.ForAll.toForall [Functor f] [Representable f] : ForAll container p ↔ ∀ (ix : Ix f), p (get container ix) := by
   simp [ForAll]
 
 /-- Attach the rest of a ForAll at every location -/
-def Foldable.attachForAll [Functor f] [Foldable f] {container : f α} {p : α → Prop} (h : ForAll container p) : f { a : α // p a } :=
+def Representable.attachForAll [Functor f] [Representable f] {container : f α} {p : α → Prop} (h : ForAll container p) : f { a : α // p a } :=
   build fun ix => ⟨get container ix, by simp at h; apply h⟩
 
 
-namespace Foldable
-variable {f : Type → Type} [Functor f] [Foldable f] {α : Type} {σ : Type}
+namespace Representable
+variable {f : Type → Type} [Functor f] [Representable f] {α : Type} {σ : Type}
 
-end Foldable
+end Representable
 
 /-
-class Traversable (f : Type → Type) [Functor f] extends Foldable f where
+class Traversable (f : Type → Type) [Functor f] extends Representable f where
   traverseM {m : Type → Type} [Applicative m] : (container : f α) → (eff : α → m β) → m (f β)
 -/
 
@@ -69,10 +69,10 @@ theorem Functor.fillUnit_fmap [Functor f] [LawfulFunctor f]
   simp [fillUnit, map_const, ← comp_map]
 
 @[simp]
-theorem Functor.fillUnit_attachForAll [Functor f] [LawfulFunctor f] [Foldable f]
-    (container : f α) {p : α → Prop} (h : Foldable.ForAll container p) : Functor.fillUnit (Foldable.attachForAll h) = Functor.fillUnit container := by
-  simp [fillUnit, map_const, ← comp_map, Foldable.attachForAll]
-  apply Foldable.get_inj
+theorem Functor.fillUnit_attachForAll [Functor f] [LawfulFunctor f] [Representable f]
+    (container : f α) {p : α → Prop} (h : Representable.ForAll container p) : Functor.fillUnit (Representable.attachForAll h) = Functor.fillUnit container := by
+  simp [fillUnit, map_const, ← comp_map, Representable.attachForAll]
+  apply Representable.get_inj
   funext ix
   simp
 
